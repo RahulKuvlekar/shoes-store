3405833abf890f0db8b47ed2f31b0670d04e2667
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-07-06 16:54:33 +0530

    Feat - Created Banner Carousel Component

---
+++ src/Utils/Data/carouselData.js
@@ -0,0 +1,34 @@
+export const carouselData = [
+  {
+    image: "/Images/BannerCarousel/1.jpeg",
+    pathname: "",
+  },
+  {
+    image: "/Images/BannerCarousel/2.jpeg",
+    pathname: "",
+  },
+  {
+    image: "/Images/BannerCarousel/3.jpeg",
+    pathname: "",
+  },
+  {
+    image: "/Images/BannerCarousel/4.jpeg",
+    pathname: "",
+  },
+  {
+    image: "/Images/BannerCarousel/5.jpeg",
+    pathname: "",
+  },
+  {
+    image: "/Images/BannerCarousel/6.jpeg",
+    pathname: "",
+  },
+  {
+    image: "/Images/BannerCarousel/7.jpeg",
+    pathname: "",
+  },
+  {
+    image: "/Images/BannerCarousel/8.webp",
+    pathname: "",
+  },
+];
